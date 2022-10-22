6719606b6fc82547080b31f5d6503bc2cf6040ba
AlexanderD <alexander.vdem@gmail.com>
2022-10-22 15:00:13 +0300

    Implement database seeding class and method.

--- PlatformService/Data/IPlatformRepo.cs
+++ PlatformService/Data/IPlatformRepo.cs
@@ -7,6 +7,8 @@ public interface IPlatformRepo
     bool SaveChanges();
 
     IEnumerable<Platform> GetAllPlatforms();
+
     Platform GetPlatformById(int id);
+    
     void CreatePlatform(Platform plat);
 }
