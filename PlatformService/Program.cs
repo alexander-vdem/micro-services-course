bae3bef18d83a4e7b7e3df593165d00414694b1d
AlexanderD <alexander.vdem@gmail.com>
2022-10-22 15:15:23 +0300

    Configure Automaper in Programcs Create read and write Platform DTOs. Map Platform Model to Platform DTOs inside PlatformsProfile.cs

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
